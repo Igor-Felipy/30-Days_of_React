ebdf407af68b8284a90486096522841b7351ee8b
Igor-Felipy <igor.felipy123@gmail.com>
2020-10-07 11:46:34 -0300

    criando components

--- 04-day/react-components/src/index.js
+++ 04-day/react-components/src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Header = () => (
+  <header>
+    <div className="header-wrapper">
+      <h1>Welcome to 30 Days os React</h1>
+      <h2>Getting Started React</h2>
+      <p>Igor Felipy</p>
+      <small>Oct 7, 2020</small>
+    </div>
+  </header>
+)
+
+const UserCard = () => (
+  <div>
+    <img/>
+    <h2>Igor Felipy</h2>
+  </div>
+)
+
+const TechList = () => {
+  const techs = ['HTML','CSS','JS']
+  const techsFormated = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormated
+}
+
+const Main = () => (
+  <main>
+    <div className="main-wrapper">
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList />
+      </ul>
+      <UserCard/>
+    </div>
+  </main>
+)
+
+const Footer = () => (
+  <footer>
+    <div className="footer-wrapper">
+      <p>Copyright 2020</p>
+    </div>
+  </footer>
+)
+
+const App = () => (
+  <div className='app'>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
